27697718087e4a4925b743a4ac7a7aff0e186ae6
Stevan Kapicic <kapicic.ste1@gmail.com>
2019-06-27 16:34:07 +0200

    feat: react-redux initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const App: React.FC = () => {
+	
   return (
-    <div className="App">
-      <h1>Hello React!</h1>
-    </div>
+		<Provider store={store} >
+			<div className="App">
+				<h1>Hello React!</h1>
+			</div>
+		</Provider>
   );
 }
 
---
+++ src/redux/store/initialState.ts
@@ -0,0 +1,8 @@
+import State from "../../models/state/State";
+
+const initialState: State = {
+	users: [],
+	isLoggedIn: false
+};
+
+export default initialState;
