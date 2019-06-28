9fce4a65c103f4838b475f9d35af413bc35b9040
Stevan Kapicic <kapicic.ste1@gmail.com>
2019-06-28 11:29:21 +0200

    feat: redux-saga

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import App2 from './App2';
 
 const App: React.FC = () => {
 	
   return (
 		<Provider store={store} >
+			<App2/>
 			<div className="App">
 				<h1>Hello React!</h1>
 			</div>
--- src/redux/reducers/userReducer.ts
+++ src/redux/reducers/userReducer.ts
@@ -1,8 +1,18 @@
 import initialState from '../store/initialState';
+import actionTypes from '../actions/actionTypes';
 
 const userReducer = (state = initialState, action: any) => {
 	switch(action.type) {
-		
+		case actionTypes.USER_FETCH_SUCCEEDED:
+			return { 
+				...state,
+				users: action.users
+			};
+		case actionTypes.USER_FETCH_FAILED:
+			return {
+				...state,
+				users: []
+			};
 		default:
 			
 		return state;
